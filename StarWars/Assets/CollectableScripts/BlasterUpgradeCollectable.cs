5f32e25cf760a56f6a5e675478192a06a3bd3b73
Matthew Irizarry <42564599+mattirizarry@users.noreply.github.com>
2023-11-03 23:45:50 -0500

    Develop (#49)
    * test scene added
    
    Added Test Scene
    Added Test Player
    
    * Player Movement Added
    
    Added Controller Script
    Added Movement Script
    
    * Added Entity Class
    
    Added Entity base class (WIP)
    
    * Animation Import
    
    * Updated Sprint3 tasks.
    
    * Initial level design
    
    - added game assets
    - created terrain & background tile maps
    - added sorting layer so background is behind terrain
    - added tilemap collider 2d to terrain tilemap
    - added static rigid body & composite 2d collider to terrain tilemap
    
    * SampleScene renamed
    
    Sample Scene Renamed to Tutorial Level Scene
    
    * Improved resolution
    
    - improved resolution by modifying assets
    
    * Character Controls Finished
    
    Added Shooting
    Finished Basic Controls
    Added Bullet sprite/Prefab
    
    * Orgin/epic/level design (#46)
    
    * Initial level design
    
    - added game assets
    - created terrain & background tile maps
    - added sorting layer so background is behind terrain
    - added tilemap collider 2d to terrain tilemap
    - added static rigid body & composite 2d collider to terrain tilemap
    
    * SampleScene renamed
    
    Sample Scene Renamed to Tutorial Level Scene
    
    * Improved resolution
    
    - improved resolution by modifying assets
    
    * Added sprites and base scripts for Collectables
    
    -collectable sprites
    -skeleton scripts for collectables
    -decorator class for collectables
    
    ---------
    
    Co-authored-by: Matthew Irizarry <42564599+mattirizarry@users.noreply.github.com>
    
    * Player Camera (#48)
    
    ---------
    
    Co-authored-by: Keimon <60746048+KeBush00@users.noreply.github.com>
    Co-authored-by: Zachary Vance <zach.a.vance@gmail.com>
    Co-authored-by: Zach <55119942+Zach-Vance@users.noreply.github.com>

---
+++ StarWars/Assets/CollectableScripts/BlasterUpgradeCollectable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlasterUpgradeCollectable : CollectableDecorator
+{
+    public int fireRate = 10;
+
+    public override void Collect(Entity player)
+    {
+        //player.IncreaseFireRate(fireRate);
+    }
+}
