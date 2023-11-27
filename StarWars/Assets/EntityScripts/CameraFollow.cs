7edc6878b5cf59390040608f5f1cda3b4aa1d0a5
Zachary Vance <zach.a.vance@gmail.com>
2023-11-27 13:13:38 -0600

    SHIT FUCK ME
    godammit

--- StarWars/Assets/EntityScripts/CameraFollow.cs
+++ StarWars/Assets/EntityScripts/CameraFollow.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
-    public float yOffset = 1f;
     public Transform target;
+    public float fixedYPosition = 0f; // Set this value in the Inspector to the desired Y-axis position
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        // Camera follows the player on the X-axis and stays at the fixed Y position
+        Vector3 newPos = new Vector3(target.position.x, fixedYPosition, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
+
+
+
